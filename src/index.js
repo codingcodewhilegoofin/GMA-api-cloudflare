805ec87d122ec06ca0f25b7c4f2c3547593b2cf9
Giovanni Moscato <69764323+codingcodewhilegoofin@users.noreply.github.com>
2022-08-16 17:03:55 -0500

    Arduino endpoint working

--- src/index.js
+++ src/index.js
@@ -10,12 +10,6 @@ import {
   ThrowableRouter,
 } from 'itty-router-extras';
 
-const testUrl = 'https://swapi.dev/api/people/1/';
-const polygonUrlBase = 'https://api.polygon.io/v1/open-close/';
-let symbol = 'AAPL';
-let date = '2022-08-03';
-let adjusted = 'true';
-
 import Tests from './handlers/tests.js';
 import Test from './handlers/test.js';
 import ApiTest from './handlers/OpenAi/apitest.js';
@@ -25,11 +19,14 @@ import StockTicker from './handlers/Financial/stock.js';
 import CryptoTicker from './handlers/Financial/cryptoTicker.js';
 import NftTicker from './handlers/Financial/nft.js';
 import GovData from './handlers/Financial/gov.js';
+import CodeLedToggleON from './handlers/Arduino/codeLedToggleON.js';
+
 
 const router = Router();
 
 router.get('/', () => new Response("Hello",{status: 200})); 
 
+// Test routes
 router.get('/api/tests', Tests)
 router.get('/api/test/:id', Test );
 
@@ -46,6 +43,10 @@ router.get('/api/Crypto/:cryptoticker/:adjusted', CryptoTicker );
 router.get('/api/Nft/:collectionName', NftTicker );
 router.get('/api/Gov', GovData );
 
+// Connect to Arduino API
+router.get('/api/Arduino/ToggleON', CodeLedToggleON );
+
+
 
 router.get('*', () => new Response('Not found bitchh', { status: 404 }));
 
@@ -53,57 +54,3 @@ router.get('*', () => new Response('Not found bitchh', { status: 404 }));
 addEventListener('fetch', event =>
   event.respondWith(router.handle(event.request))
 )
-
-//Test function 
-/* async function handleRequest(request) {
-
-  const{ symbol } = await request.json();
-  console.log("\n req:" , request.method);
-  console.log("\n JSON:" , request.json());
-
-   if (request.query.symbol) {
-    symbol = request.query.symbol;
-  } else {
-    symbol = 'AAPL';
-  } 
-
-  
-    if (request.query.date) {
-      date = request.query.date;
-    } else {
-      date = '2022-08-03';
-    }
-  
-    if (request.query.adjusted) {
-      adjusted = request.query.adjusted;
-    } else {
-      adjusted = 'true';
-    } 
-
-  try {
-    const response = await fetch((polygonUrlBase + `${symbol}/` + `${date}/` + `?adjusted=${adjusted}` + `&apiKey=${POLYGON_API_KEY}`));
-
-    if (!response.ok) {
-      const message = `Bad response: ${response.status}`;
-      //res.status(401).send("Could not find ticker symbol or other issue");
-      throw new Error(message);
-    }
-    else {
-
-      const data = await response.json();
-
-      //res.status(200).send(data);
-      return new Response(JSON.stringify(data), {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-    }
-
-  }
-  catch (error) {
-    console.error("Failed GET for stock data: " + error);
-    //res.status(401).send("Could not find ticker symbol or other issue");
-  }
-}
- */
